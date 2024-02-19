07cb0fad6da85188eed9ca3292f5ba3f746d84dc
atul55tripathi <atul55tripathi@gmail.com>
2024-02-19 15:38:34 +0530

    final try

---
+++ src/test/java/test.java
@@ -0,0 +1,39 @@
+import org.example.Main;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class test {
+
+    @Test
+    public void test_squareRoot() {
+        double val = 16.0;
+        double expectedResult = 4.0;
+        double result = Main.squareRoot(val);
+        Assert.assertEquals(expectedResult, result, 0.0f);
+    }
+
+    @Test
+    public void test_factorial() {
+        int a = 5;
+        int expectedResult = 120;
+        int result = Main.factorial(a);
+        Assert.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void test_naturalLog() {
+        double a = 2.718;
+        double expectedResult = 1.0;
+        double result = Main.naturalLog(a);
+        Assert.assertEquals(expectedResult, result, 0.2f);
+    }
+
+    @Test
+    public void test_power() {
+        double a = 2.0;
+        double b = 5.0;
+        double expectedResult = 32.0;
+        double result = Main.power(a, b);
+        Assert.assertEquals(expectedResult, result, 0.0f);
+    }
+}
